3da1c6f4ed4b4d0942ee33f6dd4ea99388146507
wesley <wesleysilvadesousa@gmail.com>
2021-05-11 11:29:39 -0300

    feat: Select bet number button

--- src/components/NumberButton/index.tsx
+++ src/components/NumberButton/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { ChooseNumber } from './styles';
+import React, { useState } from 'react'
+import { ChooseNumber, Number } from './styles';
 const NumberButton: React.FC<{range: number}> = (props) => {
     let numbers = []
     for (let i = 0; i < props.range; i++) {
         numbers.push(i)
     }
+    
+    const [selectedNumbers, setSelectedNumbers ] = useState<number[]>([]);
+
+    const handleClick = (index: number) => {
+        if (selectedNumbers.includes(index + 1)) {
+            setSelectedNumbers(prev => (
+                prev.filter((item) => item !== index + 1)
+            ))
+            return
+        }
+        setSelectedNumbers((prev) => [...prev, index + 1]);
+    }
+    
     return(
         <ChooseNumber>
-            {numbers.map((item) => <div key={item}>{+item + 1}</div>)}
+            {numbers.map((item, index) => 
+                <Number 
+                    key={item} 
+                    color={selectedNumbers.includes(index + 1) ? '#27C383' : ''} 
+                    onClick={() => handleClick(index)}>{+item + 1}
+                </Number>
+            )}
         </ChooseNumber>
     )
 }  
--- src/components/NumberButton/styles.ts
+++ src/components/NumberButton/styles.ts
@@ -3,18 +3,20 @@ import styled from 'styled-components';
 export const ChooseNumber = styled.section`
     display: flex;
 	flex-wrap: wrap;
-    div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background: #adc0c4;
-        margin: 20px 13px 20px 0px;
-        width: 65px;
-        height: 65px;
-        border-radius: 100px;
-        font-size: 1.25em;
-        font-weight: 700;
-        cursor: pointer;
-        color: #fff;
-    }
+   
 `;
+
+export const Number= styled.div<{color?:  string}>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${props => props.color ? props.color : '#adc0c4'};
+    margin: 20px 13px 20px 0px;
+    width: 65px;
+    height: 65px;
+    border-radius: 100px;
+    font-size: 1.25em;
+    font-weight: 700;
+    cursor: pointer;
+    color: '#fff'
+`
