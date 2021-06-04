2138ad9eaa57ca6162eba6942e98ceb1f51ff730
wesley <wesleysilvadesousa@gmail.com>
2021-06-04 11:51:43 -0300

    refactor: send update user

--- src/Pages/Account/index.tsx
+++ src/Pages/Account/index.tsx
@@ -1,9 +1,11 @@
 import Card from '../../components/Card';
 import Navbar from '../../components/Navbar'
-import { Container, Input } from './styles';
-import { useEffect, useState } from 'react';
+import { Container, Input, SaveButton } from './styles';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api'
 import useInput from '../../hooks/use-input';
+import MessageBox from '../../components/MessageBox';
+import Loader from 'react-loader-spinner';
 
 
 interface IUser {
@@ -13,60 +15,117 @@ interface IUser {
 
 const Account = () => {
     const [user, setUser] = useState<IUser>()
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null)
+    const [success, setSuccess] = useState<string | null>(null)
 
     useEffect(() => {
+        setLoading(true)
         const fetchUser = async () => {
             const token = localStorage.getItem('token')
             const response = await api.get<IUser>('users', { headers :  {"Authorization" : `Bearer ${token}`} })
             setUser({email: response.data.email, username: response.data.username})
         }
         fetchUser()
-    }, [])
+        setLoading(false)
+    }, [user])
 
     const {
         value: enteredName,
-        isValid: enteredNameIsValid,
         hasError: nameInputHasError,
         valueChangeHandler: nameChangedHandler,
         inputBlurHandler: nameBlurHandler,
-        reset: resetNameInput,
+    } = useInput((value) => value.trim() !== "");
+
+    const {
+        value: enteredPassword,
+        valueChangeHandler: passwordChangedHandler,
+        inputBlurHandler: passwordBlurHandler,
     } = useInput((value) => value.trim() !== "");
 
     const {
         value: enteredEmail,
-        isValid: enteredEmailIsValid,
         hasError: emailInputHasError,
         valueChangeHandler: emailChangeHandler,
         inputBlurHandler: emailBlurHandler,
-        reset: resetEmailInput,
       } = useInput((value) => value.includes("@"));
 
+    const formSubmissionHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setLoading(true)
+        setError('')
+        setSuccess('')
+        const token = localStorage.getItem('token')
+        const data: any = {}
+        if (enteredEmail !== '') {
+            data.email = enteredEmail
+        }
+        if (enteredName !== '') {
+            data.username = enteredName
+        }
+        if (enteredPassword !== '') {
+            data.password = enteredPassword
+        }
+        try {
+            await api.put('users', data, { headers :  {"Authorization" : `Bearer ${token}`} })
+            setSuccess('Alterado com suceeso')
+        } catch (error) {
+            setError('Algo de errado aconteceu')
+        }
+        
+        console.log(enteredName)
+        console.log(enteredEmail)
+        setLoading(false)
+    };
+    
     return(
         <>
         <Navbar/>
         <Container>
-            <Card onSubmit={() => {}}>
-          <Input
-            value={enteredName}
-            onChange={nameChangedHandler}
-            onBlur={nameBlurHandler}
-            placeholder="Name"
-            type="text"
-          />
-          {nameInputHasError && (
-            <p style={{ color: "red" }}>Please enter a valid name.</p>
-          )}
-          <hr />
-          <Input
-            value={enteredEmail}
-            onChange={emailChangeHandler}
-            onBlur={emailBlurHandler}
-            placeholder="Email"
-            type="email"
-          />
-          {emailInputHasError && (
-            <p style={{ color: "red" }}>Please enter a valid email.</p>
-          )}
+            <div>
+                <h2>Username: {user?.username}</h2>   
+                <h2>Email: {user?.email}</h2>   
+                {error && <MessageBox title={error} color='red'/>} 
+                {success && <MessageBox title={success} color='green'/>} 
+            </div>
+            <Card onSubmit={formSubmissionHandler}>
+                <Input
+                    value={enteredName}
+                    onChange={nameChangedHandler}
+                    onBlur={nameBlurHandler}
+                    placeholder="New username"
+                    type="text"
+                />
+                {nameInputHasError && (
+                    <p style={{ color: "red" }}>Please enter a valid name.</p>
+                )}
+                <hr />
+                <Input
+                    value={enteredEmail}
+                    onChange={emailChangeHandler}
+                    onBlur={emailBlurHandler}
+                    placeholder="New email"
+                    type="email"
+                />
+                {emailInputHasError && (
+                    <p style={{ color: "red" }}>Please enter a valid email.</p>
+                )}
+                <hr />
+                <Input
+                    value={enteredPassword}
+                    onChange={passwordChangedHandler}
+                    onBlur={passwordBlurHandler}
+                    placeholder="New password"
+                    type="password"
+                />
+                <hr />
+                <SaveButton>
+                    <h1>
+                        Save
+                        {loading && <Loader type="Oval" color="#27c383" height={20} width={20} />}
+                    </h1>
+                </SaveButton>
+
             </Card>
         </Container>
         </>
--- src/Pages/Account/styles.ts
+++ src/Pages/Account/styles.ts
@@ -2,10 +2,20 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     display: flex;
-    justify-content: center;
-    margin-top: 150px;
+	justify-content: space-evenly;
+	align-items: center;
+	margin-top: 150px;
+    h2 {
+        font-size: 20px;
+		text-align: center;
+		font-style: italic;
+    }
+	@media(max-width: 800px){
+		margin-top: 30px;
+		display: flex;
+		flex-direction: column;
+	}
 `;
-
 
 export const Input = styled.input`
 	border-radius: 14pc; 
@@ -20,3 +30,15 @@ export const Input = styled.input`
 		outline: none;
 	}
 `;
+
+export const SaveButton = styled.button`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+	text-decoration: none;
+	color: #707070;
+	background-color: #fff;		
+`
+
